3bf6ff10be87b60592bae2f828b79a163282ce5e
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-07 00:32:42 +0530

    controller

--- patient/src/main/java/com/example/patient/controller/PatientController.java
+++ patient/src/main/java/com/example/patient/controller/PatientController.java
@@ -1,8 +1,18 @@
 package com.example.patient.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+public class PatientController {
+    @PostMapping(path = "/addPatient",consumes = "application/json",produces = "application/json")
+    public String AddPatient(){
+        return "Add Patient";
+    }
+        @GetMapping("/viewPatient")
+    public String ViewPatient(){
+        return "View Patient";
+        }
 
 }
